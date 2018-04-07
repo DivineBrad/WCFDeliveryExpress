5c63a37c01f6540f7e945a7f5623df567447f21c
DivineBrad <bradleymjblanchard@gmail.com>
2018-04-07 02:40:13 -0400

    Initial Project SetUp, Command Line Host Working

---
+++ WCFDeliveryExpress/DeliveryHost/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeliveryHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            using (ServiceHost host = new ServiceHost(typeof(WCFDeliveryExpress.DeliveryService)))
+            {
+                host.Open();
+                Console.WriteLine("Host Started , Press Any Key To Stop");
+                Console.ReadLine();
+            }
+        }
+    }
+}
